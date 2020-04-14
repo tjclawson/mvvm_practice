e380e46afaf82cd3a0dfa6dd7c277851930d1449
tjclawson <tjamesclawson@gmail.com>
2020-04-13 20:54:34 -0400

    Setup UI

--- app/src/main/java/com/tjclawson/mvvm_practice/ui/MainActivity.kt
+++ app/src/main/java/com/tjclawson/mvvm_practice/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.tjclawson.mvvm_practice
+package com.tjclawson.mvvm_practice.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tjclawson.mvvm_practice.R
 
 class MainActivity : AppCompatActivity() {
 
