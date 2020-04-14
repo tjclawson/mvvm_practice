a89c324309c31a4708f7ed1ca95dd335dff78826
tjclawson <tjamesclawson@gmail.com>
2020-04-13 21:18:43 -0400

    Add fragments and setup nav graph

---
+++ app/src/main/java/com/tjclawson/mvvm_practice/ui/weather/future/list/FutureListWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.tjclawson.mvvm_practice.ui.weather.future.list
+
+import androidx.lifecycle.ViewModel
+
+class FutureListWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
