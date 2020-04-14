6ca4824820ae67c06934f1993b16d019e1146fee
tjclawson <tjamesclawson@gmail.com>
2020-04-14 16:43:58 -0400

    Add converters for conditions list, make necessary changes to CurrentWeatherEntry.kt to store in room db

--- app/src/main/java/com/tjclawson/mvvm_practice/data/db/entity/CurrentWeatherEntry.kt
+++ app/src/main/java/com/tjclawson/mvvm_practice/data/db/entity/CurrentWeatherEntry.kt
@@ -1,8 +1,13 @@
 package com.tjclawson.mvvm_practice.data.db.entity
 
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+const val CURRENT_WEATHER_ID = 0
+
+@Entity(tableName = "current_weather")
 data class CurrentWeatherEntry(
     val cloudcover: Double,
     val feelslike: Double,
@@ -29,4 +34,7 @@ data class CurrentWeatherEntry(
     val windDir: String,
     @SerializedName("wind_speed")
     val windSpeed: Double
-)
+) {
+    @PrimaryKey(autoGenerate = false)
+    var id: Int = CURRENT_WEATHER_ID
+}
