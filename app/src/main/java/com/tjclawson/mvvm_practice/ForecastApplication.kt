9a6ac1fdbf607972c14a5f3371cf538da4b26726
tjclawson <tjamesclawson@gmail.com>
2020-04-14 19:38:59 -0400

    Add current weather viewmodel implementation

--- app/src/main/java/com/tjclawson/mvvm_practice/ForecastApplication.kt
+++ app/src/main/java/com/tjclawson/mvvm_practice/ForecastApplication.kt
@@ -1,15 +1,18 @@
 package com.tjclawson.mvvm_practice
 
 import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
 import com.tjclawson.mvvm_practice.data.db.ForecastDatabase
 import com.tjclawson.mvvm_practice.data.network.*
 import com.tjclawson.mvvm_practice.data.repository.ForecastRepository
 import com.tjclawson.mvvm_practice.data.repository.ForecastRepositoryImpl
+import com.tjclawson.mvvm_practice.ui.weather.current.CurrentWeatherViewModelFactory
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.androidXModule
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
 import org.kodein.di.generic.singleton
 
 class ForecastApplication : Application(), KodeinAware {
@@ -22,6 +25,11 @@ class ForecastApplication : Application(), KodeinAware {
         bind() from singleton { WeatherstackApiService(instance()) }
         bind<WeatherNetworkDataSource>() with singleton { WeatherNetworkDataSourceImpl(instance()) }
         bind<ForecastRepository>() with singleton { ForecastRepositoryImpl(instance(), instance()) }
+        bind() from provider { CurrentWeatherViewModelFactory(instance())}
+    }
 
+    override fun onCreate() {
+        super.onCreate()
+        AndroidThreeTen.init(this)
     }
 }
--- app/src/main/java/com/tjclawson/mvvm_practice/ui/weather/current/CurrentWeatherViewModel.kt
+++ app/src/main/java/com/tjclawson/mvvm_practice/ui/weather/current/CurrentWeatherViewModel.kt
@@ -1,7 +1,12 @@
 package com.tjclawson.mvvm_practice.ui.weather.current
 
 import androidx.lifecycle.ViewModel
+import com.tjclawson.mvvm_practice.data.repository.ForecastRepository
+import com.tjclawson.mvvm_practice.internal.lazyDeferred
 
-class CurrentWeatherViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class CurrentWeatherViewModel(private val forecastRepository: ForecastRepository) : ViewModel() {
+
+    val weather by lazyDeferred {
+        forecastRepository.getCurrentWeather()
+    }
 }
